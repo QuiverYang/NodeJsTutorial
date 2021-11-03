70c2ea07968dcc7fb07bbfcbdf7dcfcc1750003e
QUIVER <QUIVER.com.tw>
2021-11-03 15:53:39 +0800

    Add - demo8

---
+++ demo8.js
@@ -0,0 +1,7 @@
+let a = null;
+let b = undefined;
+let c = NaN;
+console.log(typeof c)   // number
+console.log(a == false) // false
+console.log(b == false) // false
+console.log(c == false) // false
